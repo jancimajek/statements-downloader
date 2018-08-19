8387228b867ae28c78c07a0cce230c4064f10e2f
Jan Majek <jan.majek@ft.com>
2018-08-19 12:30:40 +0100

    🔨 Refactor secret env module

--- src/secret-env.js
+++ src/secret-env.js
@@ -8,17 +8,16 @@ debug('Vault token:', VAULT_TOKEN);
 const VAULT_ADDR = process && process.env && process.env.VAULT_ADDR ? process.env.VAULT_ADDR : 'http://127.0.0.1:8200/';
 debug('Vault address:', VAULT_ADDR);
 
-const vault = require('node-vault')({
-  apiVersion: 'v1',
-  endpoint: VAULT_ADDR,
-  token: VAULT_TOKEN
-});
 
 debug('Loading unwrap token from', process.env.UNWRAP_TOKEN_FILE);
 const UNWRAP_TOKEN = require(process.env.UNWRAP_TOKEN_FILE).wrap_info.token;
 debug('Unwrap token:', UNWRAP_TOKEN);
 
-vault.unwrap({ token: UNWRAP_TOKEN })
+require('node-vault')({
+  apiVersion: 'v1',
+  endpoint: VAULT_ADDR,
+  token: VAULT_TOKEN
+}).unwrap({ token: UNWRAP_TOKEN })
   .then(result => {
     process.secret = result.data;
     debug('Secrets loaded:', Object.keys(process.secret));
